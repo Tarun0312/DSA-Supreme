b31e0470a9eb43ea1ed092d5add91117371158d0
Tarun0312 <tarunsardana33@gmail.com>
2023-09-11 00:29:27 +0530

    Pattern printing cpp

---
+++ Week1/28August23/main.cpp
@@ -0,0 +1,134 @@
+// Pattern Printing
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    /* Print Square || Solid Square Pattern*/
+    // int n;
+    // cin>>n;
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    /* Print Rectangle*/
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<5;j++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    /* Print Hollow Rectangle */
+    // int row,col;
+    // cin>>row>>col;
+    //  for (int i = 0; i < row; i++)
+    // {
+    //     for (int j = 0; j < col; j++)
+    //     {
+    //         if (i == 0 || i == row-1)
+    //         {
+    //             cout << "* ";
+    //         }
+    //         else
+    //         {
+    //             if(j==0 || j==col-1){
+    //                 cout<<"* ";
+    //             }
+    //             else{
+    //                 cout<<"  ";
+    //             }
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    /* Print Half Pyramind */
+    // int n;
+    // cin>>n;
+    // for(int row=0;row<n;row++){
+    //     for(int col=0;col<row+1;col++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    /* Print Inverted Half Pyramind */
+    // int n;
+    // cin>>n;
+    // for(int row=0;row<n;row++){
+    //     for(int col=0;col<n-row;col++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    /* Print Numeric Half Pyramid*/
+    // int n;
+    // cin >> n;
+    // for (int row = 0; row < n; row++)
+    // {
+    //     for (int col = 0; col < row + 1; col++)
+    //     {
+    //         cout << col + 1 << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    /*Print Inverted Numeric Half Pyramid */
+    // int n;
+    // cin >> n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n - i; j++)
+    //     {
+    //         cout << j + 1 << " ";
+    //     }
+    //     cout << endl;
+    // }
+    
+    /* Hollow Square Pattern*/
+
+    // int n;
+    // cin>>n;
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         if(i==0 || i==n-1){
+    //             cout<<"* ";
+    //         }
+    //         else{
+    //             if(j==0 || j==n-1){
+    //                 cout<<"* ";
+    //             } 
+    //             else{
+    //                 cout<<"  ";
+    //             }
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+    
+    /* Hollow Inverted Half Pyramid*/
+    // int n;
+    // cin>>n;
+    // for(int row=0;row<n;row++){
+    //     for(int col=0;col<n-row;col++){
+    //         if(row == 0 || row == n-1 || col==0 || col==n-row-1){
+    //             cout<<"* ";
+    //         }
+    //         else{
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+    return 0;
+}
