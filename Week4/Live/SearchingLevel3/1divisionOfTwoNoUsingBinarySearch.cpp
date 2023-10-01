1fec50fd02cb70c0764933dfb05f05bd04e2b8e9
Tarun0312 <tarunsardana33@gmail.com>
2023-10-01 14:04:39 +0530

    Division precison added

--- Week4/Live/SearchingLevel3/1divisionOfTwoNoUsingBinarySearch.cpp
+++ Week4/Live/SearchingLevel3/1divisionOfTwoNoUsingBinarySearch.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-//I/p 29 7 o/p 4(ignorng decimal value)
+//I/p 29 7 o/p 4(ignoring decimal value)
 
 int binarySearchDivisionOfTwoNumbers(int dividend,int divisor){
     int quotient=-1;
@@ -28,17 +28,21 @@ int binarySearchDivisionOfTwoNumbers(int dividend,int divisor){
     return quotient;
 }
 
+//precisison
+
 double divisionKPrecison(int dividend,int divisor,int k){
     double quotient = binarySearchDivisionOfTwoNumbers(dividend,divisor);
 
     double step=0.1;
+    int precision = k;
 
-    {
+    for(int i=0;i<precision;i++){
         double j = quotient;
         while(j*divisor <= dividend){
             quotient = j;
             j+=step;
         }
+        step/=10;
     }
     
 
@@ -46,10 +50,13 @@ double divisionKPrecison(int dividend,int divisor,int k){
 }
 int main(){
 
-    int dividend=-1,divisor=1; 
+    int dividend=951,divisor= -341;  
     //for negative numbers use absolute to make to positive then after getting the quotient,change sign
 
-    int ans = binarySearchDivisionOfTwoNumbers(abs(dividend),abs(divisor));
+    // int ans = binarySearchDivisionOfTwoNumbers(abs(dividend),abs(divisor));
+
+    //precison
+    double ans = divisionKPrecison(abs(dividend),abs(divisor),3);
 
     //now we need to decide sign (+ve ya -ve)
     if((divisor>0 && dividend <0) || (divisor <0 && dividend >0)){
